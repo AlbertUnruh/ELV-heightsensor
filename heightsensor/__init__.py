f30a880cbb41387decb54fd192a3e9bf42cda3ce
AlbertUnruh <AlbertUnruh@pm.me>
2023-07-30 11:11:32 +0200

    add first files

---
+++ heightsensor/__init__.py
@@ -0,0 +1,5 @@
+__author__ = "Keenan M. Noack"
+__version__ = "0.0.0a"
+__license__ = "MIT"
+__copyright__ = f"Copyright 2023-present (c) {__author__}"
+__url__ = "https://github.com/AlbertUnruh/ELV-heightsensor"
